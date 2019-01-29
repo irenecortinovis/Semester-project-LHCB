51f62e3262efec664a63b2b2bc2c5b836e38ed7d
Irene Cortinovis <irenecortinovis1@gmail.com>
2019-01-29 15:19:10 +0100

    Add errors

--- MightyIT_integrated_eff_gr_rectangles.py
+++ MightyIT_integrated_eff_gr_rectangles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[15]:
 
 
 import ROOT
@@ -14,14 +14,14 @@ import itertools
 from __future__ import division
 
 
-# In[30]:
+# In[16]:
 
 
-filenames_bfix = ["530_100_patchecker.root", "530_200_patchecker.root",
+filenames_bfix = [ "nocuts_patchecker.root","530_100_patchecker.root", "530_200_patchecker.root",
                   "530_300_patchecker.root", "530_400_patchecker.root",
                   "530_500_patchecker.root", "1060_300_patchecker.root"]
-xaxis_bfix = [100*530,200*530,300*530,400*530,500*530, 300*1060]
-xaxis_bfix_labels = ["100*530","200*530","300*530","400*530","500*530","300*1060"]
+xaxis_bfix = [0,200*1060,400*1060,600*1060,800*1060,1000*1060, 600*2120]
+xaxis_bfix_labels = ["no cuts", "200*1060","400*1060","600*1060","800*1060","1000*1060", "600*2120"]
 
 
 filenamelists = [filenames_bfix]
@@ -30,7 +30,7 @@ xlabellists = [xaxis_bfix_labels]
 xlabels = ["Area in IT cuts (mm^2)"]
 
 
-# In[31]:
+# In[63]:
 
 
 def get_ghostrate(title1, title2):
@@ -38,20 +38,22 @@ def get_ghostrate(title1, title2):
     #nPV_Ghosts
     nums1 = rn.hist2array(title1, copy=True)
     nghosts = np.sum(nums1)
+    rel_errghosts = np.divide(np.sqrt(nghosts),nghosts)
     
     #nPV_Total
     nums2, edges2 = rn.hist2array(title2, return_edges=True, copy=True)
     ntot = np.sum(nums2)
+    rel_errtot = np.divide(np.sqrt(ntot),ntot)
     
     ghostrate = nghosts/ntot
-    
+    ghostrate_err = ghostrate*(np.sqrt(np.add(np.square(rel_errghosts),np.square(rel_errtot))))
+    print(ghostrate_err/ghostrate*100)
     #print nghosts, ntot
-    #check why they are slightly wrong
     
-    return ntot, ghostrate
+    return ntot, ghostrate, ghostrate_err
 
 
-# In[42]:
+# In[64]:
 
 
 #Ghost rate
@@ -93,14 +95,15 @@ for filenamelist in filenamelists:
                 current_item2 = current_item2.Get(ipathlet)
             if str(type(current_item2))=="<class '__main__.TH1D'>":
                 break
-
-        totnum, ghostratenum = get_ghostrate(current_item1, current_item2)
+        
+        totnum, ghostratenum, ghostratenum_err = get_ghostrate(current_item1, current_item2)
         ghostrate.append(ghostratenum)
         #print(filename, (ghostratenum*100))
         totnumber.append(totnum)
         labelxbar.append((filename.strip(".root")).strip("_patchecker.root"))
     axes.set_title("Ghost rate")
-    axes.scatter(xaxis, ghostrate, marker='x', linewidth=1)
+    #axes.scatter(xaxis, ghostrate, marker='x', linewidth=1)
+    axes.errorbar(xaxis, ghostrate, yerr=ghostratenum_err, fmt='x', ecolor='r', linewidth=1)
     axes.set_xticks(xaxis, minor=False)
     axes.set_xticklabels(xlabellists[filenamelists.index(filenamelist)])
     
@@ -113,7 +116,7 @@ for filenamelist in filenamelists:
 plt.show()
 
 
-# In[43]:
+# In[65]:
 
 
 def get_efficiency(title1, title2):
@@ -121,22 +124,25 @@ def get_efficiency(title1, title2):
     #nPV_reconstructed
     nums1, edges1 = rn.hist2array(title1, return_edges=True, copy=True)
     n_ed = np.sum(nums1)
+    rel_errned = np.divide(np.sqrt(n_ed),n_ed)
     
     #nPV_reconstructible
     nums2, edges2 = rn.hist2array(title2, return_edges=True, copy=True)
     n_ible = np.sum(nums2)
+    rel_errnible = np.divide(np.sqrt(n_ed),n_ed)
+
     
     efficiency = n_ed/n_ible
+    efficiency_err = efficiency*(np.sqrt(np.add(np.square(rel_errned),np.square(rel_errnible))))
     
-    return efficiency   
+    print(efficiency_err/efficiency*100)
+    return efficiency, efficiency_err
 
 
-# In[48]:
+# In[66]:
 
 
 #efficiencies
-
-
 
 commonpath = "Track/PrChecker/TTrack/"
 types = ["hasT_", "long_", "long>5GeV_"]
@@ -145,11 +151,9 @@ end_path2 = "Eta_reconstructible"
 
 
 
-
-
-fig, axes = plt.subplots(len(filenamelists),len(types))
-fig.set_figheight(4)
-fig.set_figwidth(20)
+fig, axes = plt.subplots(len(types),len(filenamelists))
+fig.set_figheight(16)
+fig.set_figwidth(10)
 fig.suptitle("Reconstruction efficiency")
 
 
@@ -164,6 +168,7 @@ for typetrack in types:
     for filenamelist in filenamelists:
         xaxis = xaxislists[filenamelists.index(filenamelist)]
         efficiencies = []
+        efficiencies_err = []
         labelxbar = []
         for filename in filenamelist:
             f = ROOT.TFile.Open(filename, 'read')
@@ -187,12 +192,15 @@ for typetrack in types:
                 if str(type(current_item2))=="<class '__main__.TH1D'>":
                     break
 
-            efficiencies.append(get_efficiency(current_item1, current_item2))
-            #print(filename, paths_list1, get_efficiency(current_item1, current_item2)*100)
+            efficiency, efficiencyerror = get_efficiency(current_item1, current_item2)
+            efficiencies.append(efficiency)
+            efficiencies_err.append(efficiencyerror)
 
         
         axes[types.index(typetrack)].set_title(typetrack.strip("_"))
-        axes[types.index(typetrack)].scatter(xaxis, efficiencies, marker='x', linewidth=1)
+        #axes[types.index(typetrack)].scatter(xaxis, efficiencies, marker='x', linewidth=1)
+        axes[types.index(typetrack)].errorbar(xaxis, efficiencies, yerr=efficiencyerror, fmt='x', ecolor='r', linewidth=1)
+
 
         axes[types.index(typetrack)].set_xlabel(xlabels[filenamelists.index(filenamelist)])
         axes[types.index(typetrack)].set_ylabel("Efficiency")
@@ -203,6 +211,12 @@ for typetrack in types:
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.4)
 
     fig.savefig("Efficiencies", bbox_inches='tight')
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
